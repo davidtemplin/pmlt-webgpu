1fa33755dbdf80d4d5738036d66c7684bdf91fb3
David Templin <dtemplin@tilled.com>
2024-09-30 12:40:12 -0500

    tweaks

---
+++ src/js/kernels/sample_material.js
@@ -0,0 +1,31 @@
+class SampleMaterialKernel {
+    #config;
+    #data;
+    #pipeline;
+
+    constructor(params) {
+        this.#config = params.config;
+        this.#data = params.data;
+    }
+
+    initialize(params) {
+        const module = params.device.createShaderModule({
+            label: 'sample material module',
+            code: SAMPLE_MATERIAL_CODE,
+        });
+
+        this.#pipeline = params.device.createComputePipeline({
+            label: 'sample material compute pipeline',
+            layout: this.#data.pipelineLayout.primary,
+            compute: {
+                module,
+            }
+        });
+    }
+
+    encode(params) {
+        params.pass.setPipeline(this.#pipeline);
+        params.pass.setBindGroup(this.#config.bindGroup.primary.index, this.#data.bindGroup.primary);
+        params.pass.dispatchWorkgroupsIndirect(this.#data.element.dispatchIndirectParameters.buffer, 16 * this.#config.queue.index.sample.material);
+    }
+}
